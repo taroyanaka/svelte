22c14d669b394516ba2f78c3a52652a41908dde2
taroyanaka <6591797+taroyanaka@users.noreply.github.com>
2023-06-23 12:09:09 +0900

    anyway From Mac

--- my-svelte-project/src/App.svelte
+++ my-svelte-project/src/App.svelte
@@ -26,6 +26,155 @@ let ORDER_BY_COLUMN = 'links.id';
 let REQ_TAG = '';
 let USER = '';
 
+
+
+const test_get_user_with_permission = () =>{
+	// 'should throw an error when name is empty',
+let REQ = { body: { name: '', password: 'password' } };
+// 'should throw an error when password is empty',
+REQ = { body: { name: 'username', password: '' } };
+// 'should throw an error when name is undefined',
+REQ = { body: { password: 'password' } };
+// 'should throw an error when password is undefined',
+REQ = { body: { name: 'username' } };
+// 'should throw an error when name is null',
+REQ = { body: { name: null, password: 'password' } };
+// 'should throw an error when password is null',
+REQ = { body: { name: 'username', password: null } };
+// 'should throw an error when name length is greater than 20',
+REQ = { body: { name: 'usernameusernameusername', password: 'password' } };
+// 'should throw an error when password length is greater than 20',
+REQ = { body: { name: 'username', password: 'passwordpasswordpassword' } };
+// 'should throw an error when name length is less than 4',
+REQ = { body: { name: 'use', password: 'password' } };
+// 'should throw an error when password length is less than 4',
+REQ = { body: { name: 'username', password: 'pass' } };
+// 'should throw an error when name contains a space',
+REQ = { body: { name: 'user name', password: 'password' } };
+// 'should throw an error when password contains a space',
+REQ = { body: { name: 'username', password: 'pass word' } };
+// 'should throw an error when name contains a full-width space',
+REQ = { body: { name: 'user　name', password: 'password' } };
+// 'should throw an error when password contains a full-width space',
+REQ = { body: { name: 'username', password: 'pass　word' } };
+// 'should not throw an error when name and password are valid',
+REQ = { body: { name: 'username', password: 'password' } };
+
+
+	// expect(user.username).toEqual('testuser');
+	user.username === 'testuser' ? "" : console.log('user.username is not testuser');
+	// expect(user.user_permission).toEqual('admin');
+	user.user_permission === 'admin' ? "" : console.log('user.user_permission is not admin');
+	// expect(user.deletable).toEqual(1);
+	user.deletable === 1 ? "" : console.log('user.deletable is not 1');
+	// expect(user.writable).toEqual(1);
+	user.writable === 1 ? "" : console.log('user.writable is not 1');
+	// expect(user.readable).toEqual(1);
+	user.readable === 1 ? "" : console.log('user.readable is not 1');
+	// expect(user.likable).toEqual(1);
+	user.likable === 1 ? "" : console.log('user.likable is not 1');
+	// expect(user.commentable).toEqual(1);
+	user.commentable === 1 ? "" : console.log('user.commentable is not 1');
+
+	req = { body: { name: 'testuser', password: 'wrongpassword' } };
+	user = get_user_with_permission(req, db);
+	// expect(user).toBeNull();
+	user === null ? "" : console.log('user is not null');
+
+
+	req = { body: { name: 'nonexistentuser', password: 'password' } };
+	user = get_user_with_permission(req, db);
+	// expect(user).toBeNull();
+	user === null ? "" : console.log('user is not null');
+
+
+	req = { body: { name: "testuser'; DROP TABLE users; --", password: 'password' } };
+	user = get_user_with_permission(req, db);
+	// expect(user).toBeNull();
+	user === null ? "" : console.log('user is not null');
+
+
+	req = { body: { name: 'testuser', password: 'password' } };
+	user = get_user_with_permission(req, db);
+	// expect(user).not.toBeNull();
+	user === null ? console.log('user is null') : "";
+}
+const test_inert_link = () =>{
+	// expect(error_check_for_insert_link(undefined)).toEqual({res: 'linkが空です', status: 400});
+	error_check_for_insert_link(undefined) === null ? console.log('error_check_for_insert_link(undefined) is null') : "";
+	// expect(error_check_for_insert_link('SELECT')).toEqual({res: 'SQLの予約語を含む場合はエラー', status: 400});
+	error_check_for_insert_link('SELECT') === null ? console.log('error_check_for_insert_link(SELECT) is null') : "";
+	// expect(error_check_for_insert_link('https::///google.co.jp')).toEqual({res: 'URLの形式が正しくありません', status: 400});
+	error_check_for_insert_link('https::///google.co.jp') === null ? console.log('error_check_for_insert_link(https::///google.co.jp) is null') : "";
+	// expect(error_check_for_insert_link('https://google.co.jp/'.repeat(1000))).toEqual({res: 'URLが長すぎます', status: 400});
+	error_check_for_insert_link('https://google.co.jp/'.repeat(1000)) === null ? console.log('error_check_for_insert_link(https://google.co.jp/.repeat(1000)) is null') : "";
+	// expect(error_check_for_insert_link('https://hogehoge.com/')).toEqual({res: '許可されていないURLです', status: 400});
+	error_check_for_insert_link('https://hogehoge.com/') === null ? console.log('error_check_for_insert_link(https://hogehoge.com/) is null') : "";
+	// expect(error_check_for_insert_link('https://www.yahoo.co.jp/')).toEqual({res: 'OK', status: 200});
+	error_check_for_insert_link('https://www.yahoo.co.jp/') === null ? console.log('error_check_for_insert_link(https://www.yahoo.co.jp/) is null') : "";
+	// expect(error_check_for_insert_link('https://www.google.co.jp/')).toEqual({res: 'OK', status: 200});
+	error_check_for_insert_link('https://www.google.co.jp/') === null ? console.log('error_check_for_insert_link(https://www.google.co.jp/) is null') : "";
+	// expect(error_check_for_insert_link('https://www.youtube.com/')).toEqual({res: 'OK', status: 200});
+	error_check_for_insert_link('https://www.youtube.com/') === null ? console.log('error_check_for_insert_link(https://www.youtube.com/) is null') : "";
+
+
+	req = { body: { name: 'testuser', password: 'password' } };
+	user = get_user_with_permission(req, db);
+	// expect(user).not.toBeNull();
+	user === null ? console.log('user is null') : "";
+	// expect(user.writable).toEqual(1);
+	user.writable === 1 ? "" : console.log('user.writable is not 1');
+	// expect(user.user_permission).toEqual('admin');
+	user.user_permission === 'admin' ? "" : console.log('user.user_permission is not admin');
+	// expect(user.username).toEqual('testuser');
+	user.username === 'testuser' ? "" : console.log('user.username is not testuser');
+	// expect(user.commentable).toEqual(1);
+	user.commentable === 1 ? "" : console.log('user.commentable is not 1');
+	// expect(user.deletable).toEqual(1);
+	user.deletable === 1 ? "" : console.log('user.deletable is not 1');
+	// expect(user.likable).toEqual(1);
+	user.likable === 1 ? "" : console.log('user.likable is not 1');
+	// expect(user.readable).toEqual(1);
+	user.readable === 1 ? "" : console.log('user.readable is not 1');
+
+
+	const link_exists = db.prepare(`SELECT * FROM links WHERE link = ?`).get('https://www.google.co.jp/');
+	// expect(link_exists).not.toBeNull();
+	link_exists === null ? console.log('link_exists is null') : "";
+	const link_not_exists = db.prepare(`SELECT * FROM links WHERE link = ?`).get('https://www.yahoo.co.jp/');
+	// expect(link_not_exists).toBeUndefined();
+	link_not_exists === undefined ? "" : console.log('link_not_exists is not undefined');
+
+
+
+	const test_error_check_for_insert_tag = () => {
+	let result = error_check_for_insert_tag(undefined);
+	result.status === false ? "" : console.log('result.status is not false');
+	result.res === 'tagが空です' ? "" : console.log('result.res is not tagが空です');
+
+	result = error_check_for_insert_tag('test!');
+	result.status === false ? ''  : console.log('result.status is not false');
+	result.res === '記号を含む場合はエラー' ? '' : console.log('result.res is not 記号を含む場合はエラー');
+
+	result = error_check_for_insert_tag('test tag');
+	result.status === false ? '' : console.log('result.status is not false');
+	result.res === '空白を含む場合はエラー' ? '' : console.log('result.res is not 空白を含む場合はエラー');
+
+	result = error_check_for_insert_tag('testlong');
+	result.status === false ? '' : console.log('result.status is not false');
+	result.res === '7文字以上はエラー' ? '' : console.log('result.res is not 7文字以上はエラー');
+
+	result = error_check_for_insert_tag('SELECT');
+	result.status === false ? '' : console.log('result.status is not false');
+	result.res === 'SQLの予約語を含む場合はエラー' ? '' : console.log('result.res is not SQLの予約語を含む場合はエラー');
+
+	result = error_check_for_insert_tag('test');
+	result.status === true ? '' : console.log('result.status is not true');
+	result.res === 'OK' ? '' : console.log('result.res is not OK');
+	};
+test_error_check_for_insert_tag();
+}
+
 const test_inert_comment = () =>{
 	const DATA_LIMIT = 100;
 
